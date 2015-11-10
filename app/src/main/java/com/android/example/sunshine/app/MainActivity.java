144e1710e76e5a6fe21dec7bdbfdb98687d49909
rdrenedo <rdrenedo@gmail.com>
2015-11-10 18:43:53 +0100

    Moved MainActivity

--- app/src/main/java/com/android/example/sunshine/app/MainActivity.java
+++ app/src/main/java/com/android/example/sunshine/app/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.android.sunshine.app;
+package com.android.example.sunshine.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.android.sunshine.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
